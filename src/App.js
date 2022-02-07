38f2033d096bf37bfb502b4d4470e9dd61967a89
leecr1215 <leecr1215@naver.com>
2022-02-07 16:57:41 +0900

    finish #1.2 useInput part Two

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
 
 function App() {
+  // maxLen이 true면 useInput 함수에서 willUpdate가 true가 됨.
+  // 즉 setValue(value) 가 됨.
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+  //const email = useInput("@");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <input placeholder="Name" type="text" {...name} />
     </div>
   );
 }
