4f16a51e2822866774fd389f91decd3b09b79a3f
leecr1215 <leecr1215@naver.com>
2022-02-09 00:04:10 +0900

    learn useRef() in #2.2 useClick

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+  const potato = useRef();
+  setTimeout(() => potato.current?.focus(), 5000);
   return (
     <div>
       <div>Hi</div>
+      <input ref={potato} type="text" placeholder="la" />
     </div>
   );
 }
