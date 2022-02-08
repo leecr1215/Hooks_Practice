1cee05afad66cd30d0e8adebff8b8cfd09b002a1
leecr1215 <leecr1215@naver.com>
2022-02-08 16:44:52 +0900

    use useTabs

--- src/App.js
+++ src/App.js
@@ -2,34 +2,37 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
 
-    if (willUpdate) {
-      setValue(value);
-    }
+// 여기서 tab이 0이라면 content[0]을 의미함
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setcurrentIndex] = useState(initialTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    chagneItem: setcurrentIndex,
   };
-  return { value, onChange };
 };
 
 function App() {
-  // maxLen이 true면 useInput 함수에서 willUpdate가 true가 됨.
-  // 즉 setValue(value) 가 됨.
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Mr.", maxLen);
-  //const email = useInput("@");
+  const { currentItem, chagneItem } = useTabs(0, content);
   return (
     <div>
-      <h1>Hello</h1>
-      <input placeholder="Name" type="text" {...name} />
+      {content.map((section, index) => (
+        <button onClick={() => chagneItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 }
