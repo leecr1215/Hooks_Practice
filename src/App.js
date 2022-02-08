87d91bcd300ee7beec5fc189ded09359e74dc980
leecr1215 <leecr1215@naver.com>
2022-02-08 20:14:47 +0900

    finish #2.1 useTitle

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const sayHello = () => {
-    console.log("hello");
-  };
-  useEffect(() => {
-    sayHello();
-  });
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-
   return (
     <div>
       <div>Hi</div>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 }
---
+++ src/useTitle/App.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import { useTitle } from "./useTitle";
+
+function App() {
+  const titleUpdater = useTitle("Loading...");
+  // setTimeout : 5초 뒤 titleUpdater 실행
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return (
+    <div>
+      <div>Hi</div>
+    </div>
+  );
+}
+
+export default App;
