8f662a53ef5eee133f11b821bc8762e38401d848
leecr1215 <leecr1215@naver.com>
2022-02-08 19:53:13 +0900

    finish #2.0 Introduction to useEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
-import { useTabs } from "./useTabs";
-
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1",
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2",
-  },
-];
+import { useState, useEffect } from "react";
 
 function App() {
-  const { currentItem, chagneItem } = useTabs(0, content);
+  const [message, setMessage] = useState("");
+  const sayHello = () => {
+    setMessage("hello");
+  };
+  useEffect(() => {
+    sayHello();
+  });
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+
   return (
     <div>
-      {content.map((section, index) => (
-        <button onClick={() => chagneItem(index)}>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 }
