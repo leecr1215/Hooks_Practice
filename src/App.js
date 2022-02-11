d789cfece85056b47ea17ca096873cac3938c15f
leecr1215 <leecr1215@naver.com>
2022-02-11 22:38:35 +0900

    finish #2.2 useClick

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { useState, useEffect, useRef } from "react";
 
 const useClick = (onClick) => {
   const element = useRef();
+  // componentWillUnmount 때 useEffect 실행됨
   useEffect(() => {
+    // if문은 componentDidMount, componentDidUpdate 때 호출
     if (typeof onClick !== "function") {
       return;
     }
     // reference 안에 element.current가 있는지 확인
-    // if문은 componentDidMount, componentDidUpdate 때 호출
     if (element.current) {
       element.current.addEventListener("click", onClick);
     }
     return () => {
+      // componentWillUnMount 일 때 return
       if (element.current) {
         element.current.removeEventListener("click", onClick);
       }
